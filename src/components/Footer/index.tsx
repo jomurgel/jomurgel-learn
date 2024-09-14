d412a65a1bc46b0468c7ae7f3fd6524ac5db0a23
Jo Murgel <dev@jomurgel.com>
2024-09-13 21:01:51 -0600

    add noai component to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
+import NoAi from '@/components/NoAI'
+
 const Footer = () => {
   return (
     <footer>
       <div>&copy; 2007-2024 &mdash; Jo Murgel</div>
-
+      <div><NoAi /></div>
       <div>
         Made with <a href="" target="_blank" rel="noreferrer noopener">next.js</a>, <a href="" target="_blank" rel="noreferrer noopener">acorn.css</a>, and hosted on <a href="" target="_blank" rel="noreferrer noopener">digitalocean</a>.
       </div>
