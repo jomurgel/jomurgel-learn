8d24256348ee6b687f108a5e45eefcc7531a36dc
Jo Murgel <dev@jomurgel.com>
2025-05-18 17:17:40 -0600

    fixes file alignment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,17 +15,17 @@ const About: React.FC = () => {
           <h1 aria-label="About Jo Murgel">
             About Me
           </h1>
-            <div data-type="image-container">
-              <Image
-                src="/about.jpg"
-                alt="A photo of tattooed legs draped over the edge of a cliff overlooking rocks and the Snake river."
-                width={600}
-                height={450}
-                priority
-                style={{ width: '100%', height: '100%' }}
-                post-image="true"
-              />
-            </div>
+          <div data-type="image-container">
+            <Image
+              src="/about.jpg"
+              alt="A photo of tattooed legs draped over the edge of a cliff overlooking rocks and the Snake river."
+              width={600}
+              height={450}
+              priority
+              style={{ width: '100%', height: '100%' }}
+              post-image="true"
+            />
+          </div>
         </header>
       </section>
 
