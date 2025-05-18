22e2a98fadd2ea59ed35910be3a4e49ce43a0701
Jo Murgel <dev@jomurgel.com>
2025-05-18 17:03:11 -0600

    updates layout styles for top header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,13 @@ const Home: React.FC = () => {
           <h1>
             Hi, I'm Jo Murgel.
           </h1>
-          <p data-type="large">
+          <p data-size="large">
             A <strong>full-stack dev</strong>, <strong>UX designer</strong>, and <strong>process efficiency expert</strong><sup>[1]</sup>.<br/>
-            I build purpose and data-driven websites and craft new processes for small business, non-profits, and enterprise clients.
+            I build purpose and data-driven websites and craft new processes for small business, non-profits, and enterprise clients<sup>[2]</sup>.
           </p>
-          <p>
-            <small>1. Among other things&hellip;</small>
+          <p data-layout="footnotes">
+            <small>1. Among other things&hellip;</small><br/>
+            <small>2. I'm also a photographer, artist, and generally cool person.</small>
           </p>
           <div data-buttons>
             <Link href="/work" data-button data-variant="primary">Portfolio</Link>
