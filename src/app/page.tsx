3ad1e1849a636a39afeb9deda567f46ef26bcc43
Jo Murgel <dev@jomurgel.com>
2026-03-17 21:27:44 -0600

    updates homepage cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,15 @@ const Home: React.FC = () => {
             Hi, I'm Jo Murgel.
           </h1>
           <p data-size="large">
-            A <strong>front-end dev</strong>, <strong>user-experience designer</strong>, and <strong>process efficiency expert</strong><sup>[1]</sup>.
-            I build bespoke purpose and data-driven websites and craft new processes for small business, non-profits, and enterprise clients<sup>[2]</sup>.
+            A <strong>UX strategist, full-stack engineer, and human-centered design advocate</strong><sup>[1]</sup>. I help organizations build digital experiences grounded in real user behavior — and build the teams and processes to sustain them<sup>[2]</sup>.
+          </p>
+         <p data-size="large">
+            All of my <em>non-traditional</em> work runs through <strong><a href="https://darkfound.com">darkfound</a></strong>, a deep strategy collective focused on building web apps and websites and thinking hard about what comes next, or through <strong><a href="https://projectacorn.space">Project Acorn</a></strong>, my long-running testing ground for new concepts, ideas, and implementations —<sup>[3]</sup> the place where I nurture experiments and make mistakes in search of the right approach.
           </p>
           <p data-layout="footnotes">
             <small>1. Among other things&hellip;</small><br/>
-            <small>2. I'm also a photographer and artist.</small>
+            <small>2. I'm also a photographer and artist.</small><br/>
+            <small>3. Take my em dash. I dare you.</small>
           </p>
           <div data-buttons>
             <Link href="/work" data-button data-variant="primary">Portfolio</Link>
